01763cb20aec245babe3fafaed78ae9102d529e4
Robert Precious <robert.precous@outlook.com>
2022-04-18 13:26:37 +0100

    initial set up

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Precious Pool</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQcZW9ub9vCKxsqhItrXbKg8L8rqU0VMk",
+  authDomain: "preciouspool.firebaseapp.com",
+  databaseURL:
+    "https://preciouspool-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "preciouspool",
+  storageBucket: "preciouspool.appspot.com",
+  messagingSenderId: "216565374605",
+  appId: "1:216565374605:web:c7e7e2d8bd1aa08d264cc4",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export const database = getDatabase(firebase);
+
+export default firebase;
